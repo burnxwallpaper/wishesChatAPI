33b8a16faeb2407ba3e3320e8f5dc14ed9ad2f90
leosiu wish <55885280+burnxwallpaper@users.noreply.github.com>
2020-03-30 21:14:56 +0800

    cors fix 2

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http, { origins: '*:*' });
+const io = require('socket.io')(http,
+  {
+    handlePreflightRequest: (req, res) => {
+      const headers = {
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+        "Access-Control-Allow-Credentials": true
+      };
+      res.writeHead(200, headers);
+      res.end();
+    }
+  }
+);
+io.set('origins', '*:*');
 const port = process.env.PORT || 4000;
 //db
 const mongoose = require('mongoose');
