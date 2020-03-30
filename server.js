c452eaa450494f08f16fb66cb05d3d9c0178fa77
leosiu wish <55885280+burnxwallpaper@users.noreply.github.com>
2020-03-30 21:21:42 +0800

    cors

--- server.js
+++ server.js
@@ -1,18 +1,6 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http,
-  {
-    handlePreflightRequest: (req, res) => {
-      const headers = {
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-        "Access-Control-Allow-Credentials": true
-      };
-      res.writeHead(200, headers);
-      res.end();
-    }
-  }
-);
+const io = require('socket.io')(http);
 io.set('origins', '*:*');
 const port = process.env.PORT || 4000;
 //db
